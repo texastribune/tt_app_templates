336f0288a6ca380187327c666f1f61ba1c5ce68e
Travis Swicegood <development@domain51.com>
2012-12-09 14:06:33 -0600

    Make sure example_usage is there automatically

--- generic/example/example/settings.py
+++ generic/example/example/settings.py
@@ -128,6 +128,9 @@ INSTALLED_APPS = (
 
     # Custom application being tested
     '{{ app_name }}',
+
+    # The app with all of the tests and any example customizations
+    'example_usage',
 )
 
 # A sample logging configuration. The only tangible logging
